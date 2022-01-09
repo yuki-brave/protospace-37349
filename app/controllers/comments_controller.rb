0184e125ec85780dcfbe5d6ad10111424356e933
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-09 14:09:37 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
-      # @comments = @prototype.comments
+      @comments = @prototype.comments
       render 'prototypes/show'
     end
 
